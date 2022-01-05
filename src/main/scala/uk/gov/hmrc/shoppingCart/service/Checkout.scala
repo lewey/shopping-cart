fb7acf5ae61e57bbe52ae3ffe82a75fd2289bc13
Lewey <johnlewis@Tests-MacBook-Pro.local>
2022-01-05 21:51:43 +0000

    step 2

--- src/main/scala/uk/gov/hmrc/shoppingCart/service/Checkout.scala
+++ src/main/scala/uk/gov/hmrc/shoppingCart/service/Checkout.scala
@@ -4,9 +4,23 @@ import uk.gov.hmrc.shoppingCart.models.{Apple, Fruit, Orange}
 
 class Checkout {
 
+  def calculateAppleOfferReduction(items: List[Fruit]): Double = {
+    val appleCount: Int = items.count(_ == Apple)
+
+    (appleCount < 2, appleCount % 2 == 0) match {
+      case (true, _) => 0
+      case (false, true) => appleCount / 2 * Apple.cost
+      case (_, _) => (appleCount - 1) / 2 * Apple.cost
+    }
+  }
+
+  def calculateOrangeOfferReduction(items: List[Fruit]): Double = {
+    val orangeCount: Int = items.count(_ == Orange)
+    if(orangeCount < 3) 0 else (orangeCount / 3) * Orange.cost
+  }
+
   def totalFruitCost(items: List[Fruit]): String = {
-    val totalCost: Double = calculateTotalCost(items)
-    convertToCurrency(totalCost)
+    convertToCurrency(calculateTotalCost(items))
   }
 
   def totalStrCost(items: List[String]): String = {
@@ -16,10 +30,17 @@ class Checkout {
     }
 
     val totalCost: Double = calculateTotalCost(allFruitItems)
+
     convertToCurrency(totalCost)
   }
 
-  private def calculateTotalCost(items: List[Fruit]): Double = items.map(_.cost).sum
+  private def calculateTotalCost(items: List[Fruit]): Double = {
+    val costWithoutReduction: Double = items.map(_.cost).sum
+    val totalReduction = calculateAppleOfferReduction(items) + calculateOrangeOfferReduction(items)
+    costWithoutReduction - totalReduction
+  }
+
+
 
   private def convertToCurrency(amount: Double): String = {
     val currencyAmount = amount / 100
--- src/test/scala/uk/gov/hmrc/shoppingCart/service/CheckoutSpec.scala
+++ src/test/scala/uk/gov/hmrc/shoppingCart/service/CheckoutSpec.scala
@@ -6,14 +6,92 @@ import uk.gov.hmrc.shoppingCart.models.{Apple, Fruit, Orange}
 class CheckoutSpec extends AnyWordSpec {
 
   "totalFruitCost" should {
-    "calculate the total cost of items from a list" in {
 
+    "calculate the cost when no discount should be applied" in{
       val checkout = new Checkout
-      val items = List(Apple, Apple, Orange, Apple)
+      val items = List(Apple, Orange)
 
       val result: String = checkout.totalFruitCost(items)
 
-      assert(result == "£2.05")
+      println(result)
+      assert(result == "£0.85")
+    }
+
+    "calculate the total cost of two apples when the apple offer is applied" in {
+
+      val checkout = new Checkout
+      val items = List(Apple, Apple)
+
+      val result: String = checkout.totalFruitCost(items)
+
+      println(result)
+      assert(result == "£0.6")
+    }
+
+    "calculate the total cost of three apples when the apple offer is applied" in {
+
+      val checkout = new Checkout
+      val items = List(Apple, Apple, Apple)
+
+      val result: String = checkout.totalFruitCost(items)
+
+      println(result)
+      assert(result == "£1.2")
+    }
+
+    "calculate the total cost of three oranges when the orange offer is applied" in {
+
+      val checkout = new Checkout
+      val items = List(Orange, Orange, Orange)
+
+      val result: String = checkout.totalFruitCost(items)
+
+      println(result)
+      assert(result == "£0.5")
+    }
+
+    "calculate the total cost of one orange when the orange offer is not applied" in {
+
+      val checkout = new Checkout
+      val items = List(Orange)
+
+      val result: String = checkout.totalFruitCost(items)
+
+      println(result)
+      assert(result == "£0.25")
+    }
+
+    "calculate the total cost of two oranges when the orange offer is not applied" in {
+
+      val checkout = new Checkout
+      val items = List(Orange, Orange)
+
+      val result: String = checkout.totalFruitCost(items)
+
+      println(result)
+      assert(result == "£0.5")
+    }
+
+    "calculate the total cost of four Oranges when the orange offer is applied" in {
+
+      val checkout = new Checkout
+      val items = List(Orange, Orange, Orange, Orange)
+
+      val result: String = checkout.totalFruitCost(items)
+
+      println(result)
+      assert(result == "£0.75")
+    }
+
+    "calculate the total cost of six Oranges when the orange offer is applied" in {
+
+      val checkout = new Checkout
+      val items = List(Orange, Orange, Orange, Orange, Orange, Orange)
+
+      val result: String = checkout.totalFruitCost(items)
+
+      println(result)
+      assert(result == "£1.0")
     }
 
     "return zero amount for an empty list" in {
@@ -26,20 +104,97 @@ class CheckoutSpec extends AnyWordSpec {
   }
 
   "totalStrCost" should {
-    "calculate the total cost of items from a list" in {
-
+    "calculate the cost when no discount should be applied" in{
       val checkout = new Checkout
-      val items = List("Apple", "Apple", "Orange", "Apple")
+      val items = List("Apple", "Orange")
 
       val result: String = checkout.totalStrCost(items)
 
-      assert(result == "£2.05")
+      println(result)
+      assert(result == "£0.85")
+    }
+
+    "calculate the total cost of two apples when the apple offer is applied" in {
+
+      val checkout = new Checkout
+      val items = List("Apple", "Apple")
+
+      val result: String = checkout.totalStrCost(items)
+
+      println(result)
+      assert(result == "£0.6")
+    }
+
+    "calculate the total cost of three apples when the apple offer is applied" in {
+
+      val checkout = new Checkout
+      val items = List("Apple", "Apple", "Apple")
+
+      val result: String = checkout.totalStrCost(items)
+
+      println(result)
+      assert(result == "£1.2")
+    }
+
+    "calculate the total cost of three oranges when the orange offer is applied" in {
+
+      val checkout = new Checkout
+      val items = List("Orange", "Orange", "Orange")
+
+      val result: String = checkout.totalStrCost(items)
+
+      println(result)
+      assert(result == "£0.5")
+    }
+
+    "calculate the total cost of one orange when the orange offer is not applied" in {
+
+      val checkout = new Checkout
+      val items = List("Orange")
+
+      val result: String = checkout.totalStrCost(items)
+
+      println(result)
+      assert(result == "£0.25")
+    }
+
+    "calculate the total cost of two oranges when the orange offer is not applied" in {
+
+      val checkout = new Checkout
+      val items = List("Orange", "Orange")
+
+      val result: String = checkout.totalStrCost(items)
+
+      println(result)
+      assert(result == "£0.5")
+    }
+
+    "calculate the total cost of four Oranges when the orange offer is applied" in {
+
+      val checkout = new Checkout
+      val items = List("Orange", "Orange", "Orange", "Orange")
+
+      val result: String = checkout.totalStrCost(items)
+
+      println(result)
+      assert(result == "£0.75")
+    }
+
+    "calculate the total cost of six Oranges when the orange offer is applied" in {
+
+      val checkout = new Checkout
+      val items = List("Orange", "Orange", "Orange", "Orange", "Orange", "Orange")
+
+      val result: String = checkout.totalStrCost(items)
+
+      println(result)
+      assert(result == "£1.0")
     }
 
     "return zero amount for an empty list" in {
       val checkout = new Checkout
 
-      val result: String = checkout.totalStrCost(List.empty[String])
+      val result: String = checkout.totalFruitCost(List.empty[Fruit])
 
       assert(result == "£0.0")
     }
